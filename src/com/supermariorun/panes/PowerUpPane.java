7f5d0d6b835c34c4e785dd890aa0db3a6732bbaf
Jiaxu Tian <j_tian@u.pacific.edu>
2019-10-28 15:09:33 -0700

    back button

--- src/com/supermariorun/panes/PowerUpPane.java
+++ src/com/supermariorun/panes/PowerUpPane.java
@@ -1,22 +1,44 @@
 package com.supermariorun.panes;
 
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+
 import com.supermariorun.main.GraphicsPane;
 import com.supermariorun.main.mainSMR;
 
-public class PowerUpPane extends GraphicsPane {
-	private mainSMR program; // you will use program to get access to
-									// all of the GraphicsProgram calls{
+import acm.graphics.GObject;
+import starter.GButton;
 
+public class PowerUpPane extends GraphicsPane {
+	private mainSMR program; 
+	private GButton BackButton;	
+	
+	public PowerUpPane(mainSMR mainSMR) {
+		super();
+		program = mainSMR;
+		BackButton = new GButton("Back", 100, 100, 80, 80);
+		BackButton.setFillColor(Color.GREEN);
+	}
 	@Override
 	public void showContents() {
 		// TODO Auto-generated method stub
-		
+		program.add(BackButton);
 	}
 
 	@Override
 	public void hideContents() {
 		// TODO Auto-generated method stub
-		
+		program.remove(BackButton);
 	}
+	@Override
+	public void mousePressed(MouseEvent e) {
+		
+		GObject obj = program.getElementAt(e.getX(), e.getY());
+		
+		if (obj == BackButton) {
 
+			program.switchToShop();
+		}
+	}
 }
+
