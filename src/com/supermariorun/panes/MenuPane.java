f91b4b1d0be87c88d405f841e42e0cb58ee4eb74
Hung le <h_le21@u.pacific.edu>
2019-10-28 17:23:23 -0700

    pipe changed

--- src/com/supermariorun/panes/MenuPane.java
+++ src/com/supermariorun/panes/MenuPane.java
@@ -18,6 +18,7 @@ public class MenuPane extends GraphicsPane {
 	private GImage shopPipe;
 	private GImage tourPipe;
 	private GImage instrucPipe;
+	
 	private GImage ShopBubble;
 	private GImage TourBubble;
 	private GImage InstructBubble;
@@ -26,15 +27,17 @@ public class MenuPane extends GraphicsPane {
 		super();
 		program = mainSMR;
 		shopPipe = new GImage(IMG_FOLDER + "pipe.png",mainSMR.getWidth()/3, mainSMR.getHeight()/1.3);
-		ShopBubble = new GImage(IMG_FOLDER + "shopBubble.png",380,400);
-		tourPipe = new GImage(IMG_FOLDER + "pipe.png",175,500);
-		TourBubble = new GImage(IMG_FOLDER + "TourBubble.png",180,400);
+		shopPipe.setSize(mainSMR.getWidth()/7, mainSMR.getHeight()/3);
+		ShopBubble = new GImage(IMG_FOLDER + "shopBubble.png",mainSMR.getWidth()/3.03, mainSMR.getHeight()/1.625);
+		tourPipe = new GImage(IMG_FOLDER + "pipe.png",mainSMR.getWidth()/6.57, mainSMR.getHeight()/1.3);
+		tourPipe.setSize(mainSMR.getWidth()/7, mainSMR.getHeight()/3);
+		TourBubble = new GImage(IMG_FOLDER + "TourBubble.png",mainSMR.getWidth()/6.388, mainSMR.getHeight()/1.625);
 		instrucPipe = new GImage(IMG_FOLDER + "pipe.png",mainSMR.getWidth()/2,mainSMR.getHeight()/1.3);
-		InstructBubble = new GImage(IMG_FOLDER + "instrucBubble.png",580,400);
+		instrucPipe.setSize(mainSMR.getWidth()/7, mainSMR.getHeight()/3);
+		InstructBubble = new GImage(IMG_FOLDER + "instrucBubble.png",mainSMR.getWidth()/1.98, mainSMR.getHeight()/1.625);
 		menuBackground = new GImage(IMG_FOLDER + "menuBack.jpg", 0, 0);
 		menuBackground.setSize(mainSMR.getWidth(), mainSMR.getHeight());
-		menuMario = new GImage(IMG_FOLDER + "menuMario.png",600,15);
-		
+		menuMario = new GImage(IMG_FOLDER + "menuMario.png",mainSMR.getWidth()/1.8, mainSMR.getHeight()/50);
 	}
 
 	@Override
@@ -64,18 +67,16 @@ public class MenuPane extends GraphicsPane {
 	@Override
 	public void mousePressed(MouseEvent e) {
 		GObject obj = program.getElementAt(e.getX(), e.getY());
-		
 		if (obj == ShopBubble ) {
 			program.switchToShop();
 		}
-		
 		else if(obj == InstructBubble)
 		{
 			program.switchToInstructions();
 		}
-		
 		else if(obj == TourBubble){
 			program.switchToTour();
 		}
+		
 	}
 }
