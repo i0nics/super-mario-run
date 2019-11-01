f4ec5d9c4024a628c19365ea36cf553f4448d044
Jiaxu Tian <j_tian@u.pacific.edu>
2019-11-01 14:22:14 -0700

    button

--- src/com/supermariorun/panes/PowerUpPane.java
+++ src/com/supermariorun/panes/PowerUpPane.java
@@ -13,27 +13,30 @@ public class PowerUpPane extends GraphicsPane {
 	private mainSMR program; 
 	private GButton BackButton;
 	private GButton MushroomButton;
+	private GButton StarButton;
 	
 	public PowerUpPane(mainSMR mainSMR) {
 		super();
 		program = mainSMR;
 		BackButton = new GButton("Back", 100, 100, 80, 80);
 		BackButton.setFillColor(Color.GREEN);
-		MushroomButton = new GButton("Mushroom", 700, 500, 150, 100);
+		MushroomButton = new GButton("Mushroom", 200, 500, 150, 100);
 		MushroomButton.setFillColor(Color.BLUE);
+		StarButton = new GButton("Star", 700, 500, 150, 100);
+		StarButton.setFillColor(Color.YELLOW);
 	}
 	@Override
 	public void showContents() {
-		// TODO Auto-generated method stub
 		program.add(BackButton);
 		program.add(MushroomButton);
+		program.add(StarButton);
 	}
 
 	@Override
 	public void hideContents() {
-		// TODO Auto-generated method stub
 		program.remove(BackButton);
 		program.remove(MushroomButton);
+		program.remove(StarButton);
 	}
 	@Override
 	public void mousePressed(MouseEvent e) {
