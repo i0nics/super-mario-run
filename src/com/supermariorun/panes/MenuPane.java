109c4644143dab1eb3fd38777c22ab39b5c811d5
Bikram <bikramchatterjee@icloud.com>
2019-11-03 04:14:11 -0800

    menu

--- src/com/supermariorun/panes/MenuPane.java
+++ src/com/supermariorun/panes/MenuPane.java
@@ -30,6 +30,8 @@ public class MenuPane extends GraphicsPane {
 		final double bubbleHeight = mainHeight/5;
 		final double pipeY = mainHeight - 130;
 		final double bubbleY = mainHeight/1.756;
+		final double labelY = mainHeight/1.6;
+		final double labelSize = mainWidth/12;
 		
 		program = mainSMR;
 	
@@ -37,22 +39,22 @@ public class MenuPane extends GraphicsPane {
 		tourPipe.setSize(pipeWidth, pipeHeight);
 		tourBubble = new GImage(IMG_FOLDER + "bubble.png", mainWidth/6.6, bubbleY);
 		tourBubble.setSize(bubbleWidth, bubbleHeight);
-		tourLabel = new GImage(IMG_FOLDER + "TourLabel.png", mainWidth/6 - 5, mainHeight/1.7 + 25);
-		tourLabel.setSize(mainWidth/12, mainHeight/12);
+		tourLabel = new GImage(IMG_FOLDER + "TourLabel.png", mainWidth/6.16, labelY);
+		tourLabel.setSize(labelSize, labelSize);
 		
 		shopPipe = new GImage(IMG_FOLDER + "pPipe.png", mainWidth/2.33, pipeY);
 		shopPipe.setSize(pipeWidth, pipeHeight);
-		shopBubble = new GImage(IMG_FOLDER + "bubble.png", mainWidth/2.33 +10, bubbleY);
+		shopBubble = new GImage(IMG_FOLDER + "bubble.png", mainWidth/2.283, bubbleY);
 		shopBubble.setSize(bubbleWidth, bubbleHeight);
-		shopLabel = new GImage(IMG_FOLDER + "shopLabel.png", mainWidth/2.8 + 110, mainHeight/1.7 + 25);
-		shopLabel.setSize(mainWidth/12, mainHeight/12);
+		shopLabel = new GImage(IMG_FOLDER + "shopLabel.png", mainWidth/2.21, labelY);
+		shopLabel.setSize(labelSize, labelSize);
 		
 		guidePipe = new GImage(IMG_FOLDER + "yPipe.png", mainWidth/1.4, pipeY);
 		guidePipe.setSize(pipeWidth, pipeHeight);
-		guideBubble = new GImage(IMG_FOLDER + "bubble.png", mainWidth/1.4 +10, bubbleY);
+		guideBubble = new GImage(IMG_FOLDER + "bubble.png", mainWidth/1.3832, bubbleY);
 		guideBubble.setSize(bubbleWidth, bubbleHeight);
-		guideLabel = new GImage(IMG_FOLDER + "guideLabel.png", mainWidth/1.34 - 10, mainHeight/1.7 + 25);
-		guideLabel.setSize(mainWidth/12, mainHeight/12);
+		guideLabel = new GImage(IMG_FOLDER + "guideLabel.png", mainWidth/1.3557, labelY);
+		guideLabel.setSize(labelSize, labelSize);
 		
 		menuBackground = new GImage(IMG_FOLDER + "menuBack.gif", 0, 0);
 		menuBackground.setSize(mainWidth, mainHeight);
