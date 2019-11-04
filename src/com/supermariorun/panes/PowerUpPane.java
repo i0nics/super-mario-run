661318bdd3dae4ba0e35b93a931bbedc118ba1d4
Jiaxu Tian <j_tian@u.pacific.edu>
2019-11-03 20:21:47 -0800

    add power up image

--- src/com/supermariorun/panes/PowerUpPane.java
+++ src/com/supermariorun/panes/PowerUpPane.java
@@ -16,8 +16,11 @@ public class PowerUpPane extends GraphicsPane {
 	private GButton BackButton;
 	private GImage BackPipe;
 	private GButton MushroomButton;
+	private GImage Mushroom;
 	private GButton StarButton;
+	private GImage Star;
 	private GButton FlowerButton;
+	private GImage Flower;
 	
 	public PowerUpPane(mainSMR mainSMR) {
 		super();
@@ -26,21 +29,26 @@ public class PowerUpPane extends GraphicsPane {
 		BackButton.setFillColor(Color.GREEN);
 		BackPipe = new GImage(IMG_FOLDER + "gPipeR.png",mainSMR.getWidth()/55, mainSMR.getHeight()/6.7);
 		BackPipe.setSize(mainSMR.getWidth()/14, mainSMR.getHeight()/8);
-		
 		MushroomButton = new GButton("Mushroom", 200, 500, 150, 100);
 		MushroomButton.setFillColor(Color.BLUE);
+		Mushroom = new GImage("../media/Power Ups/Mushroom.png",mainSMR.getWidth()/7, mainSMR.getHeight()/3);
 		FlowerButton = new GButton("Flower", 500, 500, 150, 100);
 		FlowerButton.setFillColor(Color.RED);
+		Flower = new GImage("../media/Power Ups/Flower.png",mainSMR.getWidth()/2.5, mainSMR.getHeight()/3);		
 		StarButton = new GButton("Star", 800, 500, 150, 100);
 		StarButton.setFillColor(Color.YELLOW);
+		Star = new GImage("../media/Power Ups/Star.png",mainSMR.getWidth()/1.52, mainSMR.getHeight()/3);
 	}
 	@Override
 	public void showContents() {
 		program.add(BackButton);
 		program.add(BackPipe);
 		program.add(MushroomButton);
+		program.add(Mushroom);
 		program.add(FlowerButton);
+		program.add(Flower);
 		program.add(StarButton);
+		program.add(Star);
 	}
 
 	@Override
@@ -48,8 +56,11 @@ public class PowerUpPane extends GraphicsPane {
 		program.remove(BackButton);
 		program.remove(BackPipe);
 		program.remove(MushroomButton);
+		program.remove(Mushroom);
 		program.remove(FlowerButton);
+		program.remove(Flower);
 		program.remove(StarButton);
+		program.remove(Star);
 	}
 	@Override
 	public void mousePressed(MouseEvent e) {
