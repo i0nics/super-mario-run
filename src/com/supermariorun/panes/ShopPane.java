048c4ea0b16bb60337960afaebf94ac216f47cc7
Hung le <h_le21@u.pacific.edu>
2019-11-06 20:17:08 -0800

    shop change

--- src/com/supermariorun/panes/ShopPane.java
+++ src/com/supermariorun/panes/ShopPane.java
@@ -1,8 +1,10 @@
 package com.supermariorun.panes;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
-
+import javax.swing.Timer;
 import com.supermariorun.main.GraphicsPane;
 import com.supermariorun.main.mainSMR;
 
@@ -10,34 +12,61 @@ import acm.graphics.GImage;
 import acm.graphics.GObject;
 import starter.GButton;
 
-public class ShopPane extends GraphicsPane {
+public class ShopPane extends GraphicsPane implements ActionListener {
 	private mainSMR program; 
 	public static final String IMG_FOLDER = "shopPane/";
-	private GButton PowerUpButton;
-	private GButton CharacterButton;
-	private GButton BackButton;
+	private GImage PowerUpButton;
+	private GImage CharacterButton;
+	private GImage BackButton;
 	private GImage BackPipe;
 	private GImage background;
-	private GButton InventoryButton;
+	private GImage InventoryButton;
 	private GImage toad;
-
+	private GImage backLabel;
+	private GImage inventoryLabel;
+	private GImage characterLabel;
+	private GImage powerLabel;
+	private int count = 1;
+	public Timer bubbleTimer;
+	
+	
 	public ShopPane(mainSMR mainSMR) {
 		super();
-		program = mainSMR;
-		PowerUpButton = new GButton("Power Up", 700, 500, 150, 100);
-		PowerUpButton.setFillColor(Color.BLUE);
-		CharacterButton = new GButton("Character", 200, 500, 150, 100);
-		CharacterButton.setFillColor(Color.RED);
-		BackButton = new GButton("Back", 100, 100, 80, 80);
-		BackButton.setFillColor(Color.GREEN);
-		BackPipe = new GImage(IMG_FOLDER + "gPipeR.png",mainSMR.getWidth()/55, mainSMR.getHeight()/6.7);
-		BackPipe.setSize(mainSMR.getWidth()/14, mainSMR.getHeight()/8);
+		this.program = mainSMR;
+		final double mainWidth = program.getWidth();
+		final double mainHeight = program.getHeight();
+		final double pipeWidth = mainWidth/6;
+		final double pipeHeight = mainHeight/6;
+		final double bubbleWidth = mainWidth/9;
+		final double bubbleHeight = mainHeight/5;
+		final double labelWidth = mainWidth/12;
+		final double labelHeight = mainHeight/12;
+		
+		
+		bubbleTimer = new Timer(500, this);
+		PowerUpButton = new GImage("bubble.png", 770, 200);
+		PowerUpButton.setSize(bubbleWidth*1.2, bubbleHeight*1.2);
+		CharacterButton = new GImage("bubble.png", 270, 200);
+		CharacterButton.setSize(bubbleWidth*1.2, bubbleHeight*1.2);
+		BackButton = new GImage(IMG_FOLDER + "bubble.png", 152, 30);
+		BackButton.setSize(bubbleWidth*1.2, bubbleHeight*1.2);
+		BackPipe = new GImage(IMG_FOLDER + "gPipeR.png", -50, 50);
+		BackPipe.setSize(pipeWidth, pipeHeight);
 		background = new GImage(IMG_FOLDER + "redStripes.png", 0, 0);
 		background.setSize(mainSMR.getWidth(), mainSMR.getHeight());
-		InventoryButton = new GButton("Inventory", 800, 100, 100, 100);
-		InventoryButton.setFillColor(Color.YELLOW);
-		toad = new GImage(IMG_FOLDER + "tum.png",200,100);
-		toad.setSize(mainSMR.getWidth()/2, mainSMR.getHeight()/1.5);
+		InventoryButton = new GImage("bubble.png", 975, 30);
+		InventoryButton.setSize(bubbleWidth*1.2, bubbleHeight*1.2);
+		toad = new GImage(IMG_FOLDER + "tum.png",300,100);
+		toad.setSize(mainSMR.getWidth()/2, mainSMR.getHeight()/1.2);
+		backLabel = new GImage(IMG_FOLDER + "backLabel.png", 170, 75);
+		backLabel.setSize(labelWidth*1.2, labelHeight*1.2);
+		inventoryLabel = new GImage(IMG_FOLDER + "inventoryButton.png",992,75);
+		inventoryLabel.setSize(labelWidth*1.2, labelHeight*1.2);
+		characterLabel = new GImage(IMG_FOLDER + "charactersButton.png",290,245);
+		characterLabel.setSize(labelWidth*1.2, labelHeight*1.2);
+		powerLabel = new GImage(IMG_FOLDER + "powerUpsButton.png",790,245);
+		powerLabel.setSize(labelWidth*1.2, labelHeight*1.2);
+		bubbleTimer.start();
 	}
 
 	@Override
@@ -49,6 +78,10 @@ public class ShopPane extends GraphicsPane {
 		program.add(BackPipe);
 		program.add(InventoryButton);
 		program.add(toad);
+		program.add(backLabel);
+		program.add(inventoryLabel);
+		program.add(characterLabel);
+		program.add(powerLabel);
 	}
 
 	@Override
@@ -60,6 +93,10 @@ public class ShopPane extends GraphicsPane {
 		program.remove(BackPipe);
 		program.remove(InventoryButton);
 		program.remove(toad);
+		program.remove(backLabel);
+		program.remove(inventoryLabel);
+		program.remove(characterLabel);
+		program.remove(powerLabel);
 	}
 
 	@Override
@@ -67,27 +104,55 @@ public class ShopPane extends GraphicsPane {
 		
 		GObject obj = program.getElementAt(e.getX(), e.getY());
 		
-		if (obj == PowerUpButton) {
+		if (obj == PowerUpButton || obj == powerLabel) {
 			program.playPipeSound();
 			program.switchToPowerUp();
 		}
 		
-		if (obj == CharacterButton) {
+		if (obj == CharacterButton || obj == characterLabel) {
 			program.playPipeSound();
 			program.switchToCharacter();
 		}
 		
-		if (obj == InventoryButton) {
+		if (obj == InventoryButton || obj == inventoryLabel) {
 			program.playPipeSound();
 			program.switchToInventory();
 		}
 		
-		if (obj == BackButton) {
+		if (obj == BackButton || obj == backLabel || obj == BackPipe) {
 			program.menuPane.bubbleTimer.start();
 			program.playPipeSound();
 			program.stopShopSound();
 			program.playMenuSound();
 			program.switchToMenu();
 		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (count == 1) {
+			PowerUpButton.move(0, 10);
+			powerLabel.move(0, 10);
+			CharacterButton.move(0, -10);
+			characterLabel.move(0, -10);
+			backLabel.move(0, 10);
+			BackButton.move(0, 10);
+			InventoryButton.move(0,10);
+			inventoryLabel.move(0, 10);
+		}
+		
+		if (count == 2) {
+			PowerUpButton.move(0, -10);
+			powerLabel.move(0, -10);
+			CharacterButton.move(0, +10);
+			characterLabel.move(0, +10);
+			backLabel.move(0, -10);
+			BackButton.move(0, -10);
+			inventoryLabel.move(0, -10);
+			InventoryButton.move(0,-10);
+			
+			count = 0;
+		}
+		count++;
 	}
 }
