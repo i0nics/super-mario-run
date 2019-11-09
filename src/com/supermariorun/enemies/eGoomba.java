5b1fe0482532f9b6e1197da0016fdb478bbaec32
Hung le <h_le21@u.pacific.edu>
2019-11-08 16:59:22 -0800

    goomba

--- src/com/supermariorun/enemies/eGoomba.java
+++ src/com/supermariorun/enemies/eGoomba.java
@@ -2,15 +2,17 @@ package com.supermariorun.enemies;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
 import javax.swing.Timer;
 import com.supermariorun.main.GraphicsPane;
 import com.supermariorun.main.mainSMR;
 import acm.graphics.GImage;
+import acm.graphics.GObject;
 import acm.program.GraphicsProgram;
 
-public class eGoomba extends GraphicsProgram implements ActionListener {
+public class eGoomba extends GraphicsPane implements ActionListener {
 	private mainSMR program;
 	double mainWidth ;
 	double mainHeight;
@@ -28,15 +30,16 @@ public class eGoomba extends GraphicsProgram implements ActionListener {
 		mainHeight = program.getHeight();
 		
 		goomba = new GImage(IMG_FOLDER + "goomba.png", 200, 500);
-		goomba.setSize(mainWidth/ 4, mainHeight/ 3);
+		goomba.setSize(mainWidth/4, mainHeight/3);
 		timer = new Timer(30, this);
-		add(goomba);
+		
 		addPipe();
 		goomba.move(10, 0);
 		timer.start();
 		timer.setInitialDelay(2);
 
 	}
+
 
 	public void actionPerformed(ActionEvent e) {
 	
@@ -51,8 +54,8 @@ public class eGoomba extends GraphicsProgram implements ActionListener {
 		Pipe1.setSize(mainWidth / 6, mainHeight / 3);
 		Pipe2 = new GImage("media/Pipe2.png", 1000, 500);
 		Pipe2.setSize(mainWidth / 6, mainWidth/ 3);
-		add(Pipe1);
-		add(Pipe2);
+		program.add(Pipe1);
+		program.add(Pipe2);
 	}
 
 	public void checkCollision() {
@@ -72,11 +75,26 @@ public class eGoomba extends GraphicsProgram implements ActionListener {
 			goomba.move(-10, 0);
 		}
 	}
+	
 	public void goombaDead() {
 		if(isGoombaDead = true)
 		{
 			
 		}
+	}
+	@Override
+	public void showContents() {
+		program.add(goomba);
+		
+		
+	}
+
+	@Override
+	public void hideContents() {
+		// TODO Auto-generated method stub
+		program.remove(goomba);
+		
+		
 	}
 
 		
