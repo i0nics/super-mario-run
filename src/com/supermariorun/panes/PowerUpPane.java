8363e2a1c0672c4bb8394c754661c67a9e3da68b
Hung le <h_le21@u.pacific.edu>
2019-11-15 23:20:45 -0800

    coin addition, fixed nullpointer

--- src/com/supermariorun/panes/PowerUpPane.java
+++ src/com/supermariorun/panes/PowerUpPane.java
@@ -147,7 +147,6 @@ public class PowerUpPane extends GraphicsPane implements ActionListener {
 			backLabel.move(0, 10);
 			BackButton.move(0, 10);
 			buyLabelm.move(0, 10);
-			buyLabelf.move(0, 10);
 			buyLabels.move(0, 10);
 			MushroomButton.move(0, 10);
 			StarButton.move(0, 10);
@@ -157,7 +156,6 @@ public class PowerUpPane extends GraphicsPane implements ActionListener {
 			backLabel.move(0, -10);
 			BackButton.move(0, -10);
 			buyLabelm.move(0, -10);
-			buyLabelf.move(0, -10);
 			buyLabels.move(0, -10);
 			MushroomButton.move(0, -10);
 			StarButton.move(0, -10);
