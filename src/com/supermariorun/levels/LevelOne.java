976a33e2b9ac0e0318533fcf047dfc8e779760bf
Jiaxu Tian <j_tian@u.pacific.edu>
2019-11-20 22:26:14 -0800

    pipe

--- src/com/supermariorun/levels/LevelOne.java
+++ src/com/supermariorun/levels/LevelOne.java
@@ -36,6 +36,12 @@ public class LevelOne implements ILevel{
 		coins.clear();
 		Background.setLocation(0, 0);
 		
+		
+		
+		for (Pair<Integer, Integer> loop : levelMap.get("PIPE")) {
+			Environment.add(new GImage(IMG_FOLDER + "pipe.png", loop.getKey(), loop.getValue()));
+		}
+	
 		Environment.add(new GImage(IMG_FOLDER + "grassStrip.png", -2, 578));
 		Environment.add(new GImage(IMG_FOLDER + "grassStripShort.png", 2065, 578));
 		Environment.add(new GImage(IMG_FOLDER + "grassStripShort.png",3300, 578));
@@ -45,10 +51,6 @@ public class LevelOne implements ILevel{
 		Environment.add(new GImage(IMG_FOLDER + "grassStripShort.png",814, 541));
 		Environment.add(new GImage(IMG_FOLDER + "grassStripShort.png",814, 505));
 		
-		for (Pair<Integer, Integer> loop : levelMap.get("PIPE")) {
-			Environment.add(new GImage(IMG_FOLDER + "pipe.png", loop.getKey(), loop.getValue()));
-		}
-	
 		for (Pair<Integer, Integer> loop : levelMap.get("BRICK")) {
 			Environment.add(new GImage(IMG_FOLDER + "brick.png", loop.getKey(), loop.getValue()));
 		}
