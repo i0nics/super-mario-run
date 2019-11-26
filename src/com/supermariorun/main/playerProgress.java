36c5caf7af23e07f4eaedae92968734a602d701a
Bikram <bikramchatterjee@icloud.com>
2019-11-25 21:00:07 -0800

    mm

--- src/com/supermariorun/main/playerProgress.java
+++ src/com/supermariorun/main/playerProgress.java
@@ -2,7 +2,6 @@ package com.supermariorun.main;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import com.supermariorun.panes.TourPane;
 
 public class playerProgress {
